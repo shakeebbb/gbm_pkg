0e4dee8f3f04f25cb48feacd55ae1556451169f9
ohrad <erush91@gmail.com>
2019-06-17 16:59:04 -0600

    Creating nodes and connecting them, TODO:Keeping track of explored and unexplored edge angles

--- src/gbm_node.cpp
+++ src/gbm_node.cpp
@@ -11,17 +11,6 @@
 using namespace std; 
 namespace plt = matplotlibcpp;
 
-void update_window(const double x, const double y, const double t,
-                   std::vector<double> &xt, std::vector<double> &yt)
-{
-    const double target_length = 300;
-    const double half_win = (target_length/(2.*sqrt(1.+t*t)));
-
-    xt[0] = x - half_win;
-    xt[1] = x + half_win;
-    yt[0] = y - half_win*t;
-    yt[1] = y + half_win*t;
-}
 
 class point
 {
@@ -37,20 +26,26 @@ public:
 
 int id;
 point position;
+vector<float> exploredEdgeAngles; // Angles to the children or angle from the parent
+vector<float> unexploredEdgeAngles; // Unexplored angles for parent
 int nEdges;
 };
 
 point currentPosition;
 int currentNEdges = 0;
 int currentNNodes = 0;
-float sRadius = 5;
+int currentNodeId = -1;
+float sRadius = 10;
 vector<vector<node> > currentAdj;
+vector<float> currentEdgeAngles;
+bool updatePlot = false;
 
 void addEdge(vector<node> [], node, node);
 void junctionCb(const std_msgs::Bool&);
 void odomCb(const nav_msgs::Odometry&);
 void nEdgesCb(const std_msgs::Int32&);
-bool checkNodeExistence(node&);
+void edgeAnglesCb(const std_msgs::Float32MultiArray&);
+bool checkNodeExistence(node&, int&);
 void addNode(node);
 
 int main(int argc, char **argv)
@@ -59,51 +54,92 @@ int main(int argc, char **argv)
 
   ros::NodeHandle nh;
 
-  ros::Subscriber junctionSub = nh.subscribe("/X1/node_skeleton/at_a_junction", 10, junctionCb);
-  ros::Subscriber odomSub = nh.subscribe("/X1/odometry", 10, odomCb);
-  ros::Subscriber nEdgesSub = nh.subscribe("/X1/node_skeleton/number_of_edges", 10, nEdgesCb);
-  
-    int n = 1000;
-    std::vector<double> x, y;
+  ros::Subscriber junctionSub = nh.subscribe("/X1/node_skeleton/at_a_junction", 5, junctionCb);
+  ros::Subscriber odomSub = nh.subscribe("/X1/odometry", 5, odomCb);
+  ros::Subscriber nEdgesSub = nh.subscribe("/X1/node_skeleton/number_of_edges", 5, nEdgesCb);
+	//ros::Subscriber edgeAnglesSub = nh.subscribe("/X1/node_skeleton/edge_list", 10, edgeAnglesCb);
+ 
+   // int n = 1000;
+    
+   // std::vector<double> xt(2), yt(2);
 
-    const double w = 0.05;
-    const double a = n/2;
-
-    for (int i=0; i<n; i++) {
-        x.push_back(i);
-        y.push_back(a*sin(w*i));
-    }
-
-    std::vector<double> xt(2), yt(2);
-
-    plt::title("Tangent of a sine curve");
-    plt::xlim(x.front(), x.back());
-    plt::ylim(-a, a);
-    plt::axis("equal");
-
-    // Plot sin once and for all.
-    plt::named_plot("sin", x, y);
-
-    // Prepare plotting the tangent.
-    plt::Plot plot("tangent");
-
-    plt::legend();
-
+/*
     for (int i=0; i<n; i++) {
         if (i % 10 == 0) {
-            update_window(x[i], y[i], a*w*cos(w*x[i]), xt, yt);
+            //update_window(i, xt, yt);
 
+						xt[0] = 56.92;
+						//xt[1] = (i+1)*10;
+						yt[0] = -39.9844;
+						//yt[1] = (i+1)*10;
+					
             // Just update data for this plot.
-            plot.update(xt, yt);
+						plt::scatter(xt,yt);
+            //plot.update(xt, yt);
 
             // Small pause so the viewer has a chance to enjoy the animation.
             plt::pause(0.1);
         }
+ros::spinOnce();
    }
-	    
- // ros::spin();
+*/	   
 
+	while(1)
+	{
+		plt::clf();
+		for (int i = 0; i < currentNNodes; i++)
+		{
+			for (int j = 0; j < currentAdj[i].size(); j++)
+			{
+			std::vector<double> xt(2), yt(2);
+			//xt.push_back(currentAdj[currentNNodes][0].position.x);
+			//yt.push_back(currentAdj[currentNNodes][0].position.y);
+			xt[0] = currentAdj[i][0].position.x;
+			xt[1] = currentAdj[i][j].position.x;
+
+			yt[0] = currentAdj[i][0].position.y;
+			yt[1] = currentAdj[i][j].position.y;
+
+			//xt.push_back(currentAdj[i][0].position.x);
+			//yt.push_back(currentAdj[i][0].position.y);
+
+			//cout << xt.at(0) << " , " << yt.at(0) << endl; 
+
+			//plt::scatter(xt, yt, 15);
+			plt::plot(xt,yt);
+			plt::xlim(0, 200);
+			plt::ylim(-200, 200);
+
+			xt[0] = currentAdj[i][0].position.x;
+			yt[0] = currentAdj[i][0].position.y;
+			xt[1] = currentAdj[i][0].position.x;
+			yt[1] = currentAdj[i][0].position.y;
+
+			plt::scatter(xt, yt, 15);
+
+			plt::pause(0.1);
+
+			updatePlot = false;
+			}
+	}
+
+  ros::spinOnce();
+	}
   return 0;
+}
+
+void edgeAnglesCb(const std_msgs::Float32MultiArray& msg)
+{
+	cout << "Here" << endl;
+	currentEdgeAngles.clear();
+
+	try{
+	 for(int i = 0; i < currentNEdges; i++)
+	 currentEdgeAngles.push_back(msg.data[i]);
+	}
+	catch(std::exception& e){
+		cout << "Exception Caught" << endl;
+	}
 }
 
 void nEdgesCb(const std_msgs::Int32& msg)
@@ -113,14 +149,23 @@ currentNEdges = msg.data;
 
 void junctionCb(const std_msgs::Bool& msg)
 {
+	cout << endl;
+	ROS_INFO("Junction Callback Called");
+	cout << "currentNEdges = " << currentNEdges << " : " << "at_a_junction = " << (msg.data == 1) << " currentNNodes = " << currentNNodes <<endl;
+
 	if (msg.data && currentNEdges > 2)
 	{
+	cout << "Junction Detected at " << "(" <<  currentPosition.x <<  "," << currentPosition.y  << ")" << endl;
 	node tempNode;
 	tempNode.position= currentPosition;
 	tempNode.nEdges = currentNEdges;
+	//tempNode.unexploredEdgeAngles.push_back();
 
-		if(checkNodeExistence(tempNode))
+	int closestNodeId;
+		if(!checkNodeExistence(tempNode, closestNodeId))
 		addNode(tempNode);
+		else
+		currentNodeId = closestNodeId;
 	}
 }
 
@@ -134,7 +179,18 @@ void addNode(node tempNode)
 {
 vector<node> tempVector;
 tempVector.push_back(tempNode);
+	if(currentNodeId > -1)
+	{
+	tempVector.push_back(currentAdj[currentNodeId][0]);
+	currentAdj[currentNodeId].push_back(tempNode);
+	}
 currentAdj.push_back(tempVector);
+
+currentNodeId = currentNNodes;
+currentNNodes += 1;
+
+cout << "Node Added" << endl;
+updatePlot = true;
 }
 
 void addEdge(node u, node v) 
@@ -143,13 +199,19 @@ currentAdj[u.id].push_back(v);
 currentAdj[v.id].push_back(u); 
 } 
 
-bool checkNodeExistence(node& tempNode)
+bool checkNodeExistence(node& tempNode, int& closestNodeId)
 {
 	for (int i=0; i<currentNNodes; i++)
 	{
-	 if(pow(currentAdj[i][0].position.x - tempNode.position.x, 2) + pow(currentAdj[i][0].position.y - tempNode.position.y, 2) 
-	 			< pow(sRadius, 2) && currentAdj[i][0].nEdges == tempNode.nEdges)
-	 return true;
+		float d = pow(currentAdj[i][0].position.x - tempNode.position.x, 2) + pow(currentAdj[i][0].position.y - tempNode.position.y, 2);
+
+	 //if(d < pow(sRadius, 2) && (currentAdj[i][0].nEdges == tempNode.nEdges)
+	if(d < pow(sRadius, 2))
+		{
+		 cout << "Distance to closest node = " << sqrt(d) << endl;
+		 closestNodeId = i;
+		 return true;
+		}
 	}
 	return false;
 }
